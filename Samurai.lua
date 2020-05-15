2230949e70b6fba369e16eaf7cb1878986417d90
Wheel5 <allentheskink16@gmail.com>
2020-05-14 20:55:46 -0400

    added demo for objects, comments explaining how addon loading and such works

---
+++ Samurai.lua
@@ -0,0 +1,28 @@
+SAMURAI = SAMURAI or { } -- global namespace
+local sam = SAMURAI -- local copy for efficiency
+
+local EM = GetEventManager() -- local copy of event manager
+
+sam.name = "Samurai"
+sam.version = "1.0"
+
+sam.defaults = { } -- used for saved vars eventually
+
+function sam.testObjects()
+	local obj1 = sam.MasterTrial:New("obj1", "localvar1") -- create 2 objects with different values
+	local obj2 = sam.MasterTrial:New("obj2", "localvar2")
+	local testList = { obj1, obj2 } -- place objects in table to iterate over
+	
+	for _,o in pairs(testList) do -- polymorphism!
+		o:printInfo()
+	end
+end
+
+function sam.init(e, addon)
+	if addon ~= sam.name then return end -- make sure we're loading this addon
+	EM:UnregisterForEvent(sam.name.."Load", EVENT_ADD_ON_LOADED) -- we're loaded, unregister
+	SLASH_COMMANDS["/samurai"] = sam.testObjects -- register slash command for the test function
+end
+
+-- register addon load
+EM:RegisterForEvent(sam.name.."Load", EVENT_ADD_ON_LOADED, sam.init)
---
+++ src/TrialObj.lua
@@ -0,0 +1,25 @@
+SAMURAI = SAMURAI or { } -- get our namespace 
+local sam = SAMURAI -- local version again
+
+sam.MasterTrial = ZO_Object:Subclass() -- make our object
+
+local var2 = "static var" -- equivalent to static in java and such
+
+function sam.MasterTrial:New(objnum, var1) -- when called with a :, functions silently pass 'self' as the first var
+	local trial = ZO_Object.New(self) -- with a ., you have to pass self manually
+	trial:Initialize(objnum, var1)
+	return trial
+end
+
+function sam.MasterTrial:Initialize(objnum, var1)
+	self.localvar = var1
+
+	local function getObjName(objnum) -- internal function, if declared outside as local it would be 'private'
+		df("creating object %s", tostring(objnum)) -- df is equivalent to string.format
+	end
+	getObjName(objnum)
+end
+
+function sam.MasterTrial:printInfo()
+	df("static var: %s, local var: %s", var2, self.localvar) -- can access static and object variables ezpz
+end
