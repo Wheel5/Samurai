2923b3577e60dac1eb43dbf200214f5896f166b3
Wheel5 <allentheskink16@gmail.com>
2020-05-16 20:36:46 -0400

    added LibUnits and testHarness file

---
+++ testHarness.lua
@@ -0,0 +1,10 @@
+-------------------------------------------------------------------------------
+-- Contains various tests that can be run to experiment with UI or simulate
+-- different combat situations to make sure the addon functions as expected
+-------------------------------------------------------------------------------
+
+SAMURAI = SAMURAI or { }
+TEST_HARNESS = TEST_HARNESS or { }
+local sam = SAMURAI
+local th = TEST_HARNESS
+
